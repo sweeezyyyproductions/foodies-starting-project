695f0a0b083dc81eb5fc9198d18c9abd87d22453
Michael Simpson <100339112+sweeezyyyproductions@users.noreply.github.com>
2024-04-21 22:38:29 -0700

    Update meal details page heading

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function MealDetails({ params }) {
     return (
         <main>
-            <h1>test 1</h1>
+            <h1>test</h1>
             <p>
                 {params.slug}
             </p>
