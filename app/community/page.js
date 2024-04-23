4e0408b18e36e43bef3a9fdce5b2957541dcbb4f
Michael Simpson <100339112+sweeezyyyproductions@users.noreply.github.com>
2024-04-23 01:08:13 -0700

    Refactor main-header component and layout styles sections 101-106

--- app/community/page.js
+++ app/community/page.js
@@ -1,20 +1,40 @@
-import Link from 'next/link';
+import Image from 'next/image';
+
+import mealIcon from '@/assets/icons/meal.png';
+import communityIcon from '@/assets/icons/community.png';
+import eventsIcon from '@/assets/icons/events.png';
+import classes from './page.module.css';
+
 export default function CommunityPage() {
-    return (
-        <main>
-            <h1>Community Page</h1>
-            <p>
-                Check out the <Link href="/meals">Meals Page</Link> to learn more about our meals!
-                </p>
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          One shared passion: <span className={classes.highlight}>La Sistas</span>
+        </h1>
+        <p>Join our community and share your favorite recipes!</p>
+      </header>
+      <main className={classes.main}>
+        <h2>Community Perks</h2>
 
-
-                
-
-                <p>
-                Check out the <Link href="meals/share">Share Page</Link> to spread the word about us!
-                </p>
-
-                
-        </main>
-    );
+        <ul className={classes.perks}>
+          <li>
+            <Image src={mealIcon} alt="A delicious meal" />
+            <p>Share & discover recipes</p>
+          </li>
+          <li>
+            <Image src={communityIcon} alt="A crowd of people, cooking" />
+            <p>Find new friends & like-minded people</p>
+          </li>
+          <li>
+            <Image
+              src={eventsIcon}
+              alt="A crowd of people at a cooking event"
+            />
+            <p>Participate in exclusive events</p>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
 }
